b859c20d4626e1a1e646411873c222482e5271bc
rio.sumandal0907 <rio.sumandal0907@gmail.com>
2024-07-15 16:15:05 +0800

    Update Fitness App API

--- routes/workout.js
+++ routes/workout.js
@@ -5,10 +5,14 @@ const { verify } = require("../auth");
 
 const router = express.Router();
 
-router.post("/", verify, workoutController.addItem);
+router.post("/addWorkout", verify, workoutController.addItem);
 router.get("/getMyWorkouts", verify, workoutController.getAllWorkouts);
 router.patch("/updateWorkout/:id", verify, workoutController.updateWorkout);
-router.patch("/completeWorkout/:id", verify, workoutController.completeWorkout);
+router.patch(
+  "/completeWorkoutStatus/:id",
+  verify,
+  workoutController.completeWorkout
+);
 router.delete("/deleteWorkout/:id", verify, workoutController.deleteWorkout);
 
 module.exports = router;
